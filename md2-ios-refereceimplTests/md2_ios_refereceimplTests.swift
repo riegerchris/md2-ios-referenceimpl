78d3260f2c020685c5e34a343deb26388128e1c4
riegerchris <rieger.chris@googlemail.com>
2015-07-21 15:36:37 +0200

    Initial Commit

---
+++ md2-ios-refereceimplTests/md2_ios_refereceimplTests.swift
@@ -0,0 +1,36 @@
+//
+//  md2_ios_refereceimplTests.swift
+//  md2-ios-refereceimplTests
+//
+//  Created by Christoph Rieger on 21.07.15.
+//  Copyright (c) 2015 Christoph Rieger. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class md2_ios_refereceimplTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
