fbefe60fd30af1d8906b7eb9a1a1fcfa4b4be96c
riegerchris <rieger.chris@googlemail.com>
2015-09-24 12:21:10 +0200

    Add simple action documentation

--- md2-ios-refereceimpl/controller/action/MD2EnableAction.swift
+++ md2-ios-refereceimpl/controller/action/MD2EnableAction.swift
@@ -6,21 +6,36 @@
 //  Copyright (c) 2015 Christoph Rieger. All rights reserved.
 //
 
+/// Action to enable a view element.
 class MD2EnableAction: MD2Action {
     
+    /// Unique action identifier.
     let actionSignature: String
     
+    /// The view element to enable.
     let viewElement: MD2Widget
     
+    /**
+        Default initializer.
+    
+        :param: actionSignature The action identifier.
+        :param: viewElement The view element to enable.
+    */
     init(actionSignature: String, viewElement: MD2Widget) {
         self.actionSignature = actionSignature
         self.viewElement = viewElement
     }
     
+    /// Execute the action commands: Enable a view element.
     func execute() {
         self.viewElement.enable()
     }
     
+    /**
+        Compare two action objects.
+    
+        :param: anotherAction The action to compare with.
+    */
     func equals(anotherAction: MD2Action) -> Bool {
         return actionSignature == anotherAction.actionSignature
     }
--- md2-ios-refereceimpl/controller/action/MD2WebServiceCallAction.swift
+++ md2-ios-refereceimpl/controller/action/MD2WebServiceCallAction.swift
@@ -6,21 +6,40 @@
 //  Copyright (c) 2015 Christoph Rieger. All rights reserved.
 //
 
+/**
+    Action to call an external web service.
+    *NOTICE* Not implemented/specified yet.
+*/
 class MD2WebServiceCallAction: MD2Action {
     
+    /// Unique action identifier.
     let actionSignature: String
     
-    let webServiceCall: String // TODO WebServiceCall
+    /// The web service call (not fully specified yet)
+    let webServiceCall: String
     
+    /**
+        Default initializer.
+    
+        :param: actionSignature The action identifier.
+        :param: webServiceCall The web service call.
+    */
     init(actionSignature: String, webServiceCall: String) {
         self.actionSignature = actionSignature
         self.webServiceCall = webServiceCall
     }
     
+    /// Execute the action commands: Call an external web service.
     func execute() {
         // TODO
+        fatalError("Arbitrary web service calls are not implemented")
     }
     
+    /**
+        Compare two action objects.
+    
+        :param: anotherAction The action to compare with.
+    */
     func equals(anotherAction: MD2Action) -> Bool {
         return actionSignature == anotherAction.actionSignature
     }
